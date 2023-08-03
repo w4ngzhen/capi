b7a2e56835a059260655e9c3320a3c26f59f88ce
w4ngzhen <w4ngzhen@hotmail.com>
2023-08-03 09:37:16 +0800

    feat: 添加工具面板显示，支持指针所在像素颜色等信息展示。

--- layer/explore_layer.cpp
+++ layer/explore_layer.cpp
@@ -72,8 +72,11 @@ void ExploreLayer::paintAssistantPanelAt(int panelX, int panelY,
   // 鼠标位置
   int mX = this->mouse_pos_.x();
   int mY = this->mouse_pos_.y();
+  // 由于mouse_pos是逻辑坐标，为了获取图片上对应的值，需要转为实际坐标
+  int realX = mX * this->screen_scale_.width();
+  int realY = mY * this->screen_scale_.height();
   // 获取鼠标指定位置的颜色
-  QRgb rgbAtMousePos = this->screen_pic_->pixel(mX, mY);
+  QRgb rgbAtMousePos = this->screen_pic_->pixel(realX, realY);
 
   // 2 鼠标所在位置周围30px（逻辑像素）的区域内容
   const int PADDING = 30;
@@ -81,10 +84,12 @@ void ExploreLayer::paintAssistantPanelAt(int panelX, int panelY,
   int top = mY - PADDING;
   // +1是因为要把鼠标所在位置的1个像素加上
   QRect logicRect = QRect(left, top, PADDING * 2 + 1, PADDING * 2 + 1);
-  QRect partialRect = paint_helper::rectLogicPixelToRealPixel(
+  // 将逻辑像素下的Rect转为真实图像上的Rect
+  QRect realPicRect = paint_helper::rectLogicPixelToRealPixel(
       logicRect, this->screen_scale_.width(), this->screen_scale_.height());
 
-  QImage partialImage = this->screen_pic_->copy(partialRect);
+  // 从中取得对应区域的图像内容
+  QImage partialImage = this->screen_pic_->copy(realPicRect);
 
   // 3 辅助面板矩形数据
   const int ASSISTANT_PANEL_W = this->assistant_panel_size_.width();
@@ -118,11 +123,10 @@ void ExploreLayer::paintAssistantPanelAt(int panelX, int panelY,
   // SCALED_IMG_W为奇数
   const int IMG_CENTER_POINT_X = SCALED_IMG_X + (SCALED_IMG_W - 1) / 2;
   const int IMG_CENTER_POINT_Y = SCALED_IMG_Y + (SCALED_IMG_W - 1) / 2;
-  // 计算放大倍数
-  qreal zoomInScale = SCALED_IMG_W / partialRect.width();
   // 设置反色模式
+  // 需要将Pen的颜色设置为white，然后使用光栅的颜色叠加（AndNot）
   painter.setCompositionMode(QPainter::RasterOp_SourceAndNotDestination);
-  painter.setPen(QPen(Qt::white, zoomInScale));
+  painter.setPen(QPen(Qt::white, 2));
   // 设置十字路径并绘制
   QPainterPath path;
   const int EXPAND_LENGTH = 20;
@@ -131,6 +135,48 @@ void ExploreLayer::paintAssistantPanelAt(int panelX, int panelY,
   path.moveTo(IMG_CENTER_POINT_X - EXPAND_LENGTH, IMG_CENTER_POINT_Y);
   path.lineTo(IMG_CENTER_POINT_X + EXPAND_LENGTH, IMG_CENTER_POINT_Y);
   painter.drawPath(path);
+  // 绘制反色十字完成后，一定要将反色模式取消，
+  // 否则后续涂色的时候，在白色背景上一直是黑色
+  painter.setCompositionMode(QPainter::CompositionMode_Source);
 
+  // 4. 在下方涂写十字中心捕获的颜色
+  // 4.1 区域中填充颜色
+  QBrush brush(rgbAtMousePos);
+  const int COLOR_DISPLAY_RECT_W = SCALED_IMG_W;
+  const int COLOR_DISPLAY_RECT_H = 25;
+  const int COLOR_DISPLAY_RECT_X = SCALED_IMG_X;
+  const int COLOR_DISPLAY_RECT_Y = SCALED_IMG_Y + SCALED_IMG_H + BORDER_W;
+  QRect colorDisplayRect(COLOR_DISPLAY_RECT_X, COLOR_DISPLAY_RECT_Y,
+                         COLOR_DISPLAY_RECT_W, COLOR_DISPLAY_RECT_H);
+  painter.fillRect(colorDisplayRect, QBrush(rgbAtMousePos));
+  // 4.2 使用反色，在区域右侧填充色彩信息
+  const QColor color(rgbAtMousePos);
+  const QString rgbDesc = QString("rgb(%1, %2, %3)")
+                              .arg(color.red(), 3, 10, QChar('0'))
+                              .arg(color.green(), 3, 10, QChar('0'))
+                              .arg(color.blue(), 3, 10, QChar('0'));
+  // 再次使用反色
+  painter.setPen(QPen(Qt::white, 1));
+  painter.setCompositionMode(QPainter::RasterOp_SourceAndNotDestination);
+  painter.drawText(colorDisplayRect, Qt::AlignVCenter | Qt::AlignRight, rgbDesc);
+  // 恢复颜色
+  painter.setCompositionMode(QPainter::CompositionMode_Source);
+
+  // 5. 在色彩卡下方绘制坐标，以及RGB文字
+  // 5.1 准备好区域
+  const int INFO_DESC_RECT_W = SCALED_IMG_W;
+  const int INFO_DESC_RECT_H = 40;
+  const int INFO_DESC_RECT_X = SCALED_IMG_X;
+  const int INFO_DESC_RECT_Y = COLOR_DISPLAY_RECT_Y + COLOR_DISPLAY_RECT_H;
+  QRect infoDescRect(INFO_DESC_RECT_X, INFO_DESC_RECT_Y, INFO_DESC_RECT_W,
+                     INFO_DESC_RECT_H);
+  // 5.2 准备好文字
+  const QString posDesc = QString("x = %1, y = %2")
+                              .arg(QString::number(mX))
+                              .arg(QString::number(mY));
+  painter.setPen(QPen(Qt::black));
+  painter.drawText(infoDescRect, Qt::AlignHCenter | Qt::AlignVCenter, posDesc);
+
+  // finally
   painter.restore();
 }
