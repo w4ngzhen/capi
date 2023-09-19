fc8d3b579d4f03d764f4d986b36789272774e3fc
w4ngzhen <w4ngzhen@hotmail.com>
2023-09-19 13:04:03 +0800

    feat: 矩形图形支持ShapeConfig.is_filled配置效果渲染。

--- src/core/layer/captured/shape/rect_shape.cpp
+++ src/core/layer/captured/shape/rect_shape.cpp
@@ -12,7 +12,9 @@ void RectShape::onContentPaint(Painter *painter) {
   painter->save();
   const auto &contentRect = this->content_rect();
   auto &configRef = this->config_;
-  painter->fillRect(contentRect, Brush(configRef.brush_color()));
+  if (config_.is_filled()) {
+    painter->fillRect(contentRect, Brush(configRef.brush_color()));
+  }
   painter->setPen(Pen(configRef.pen_color(), configRef.pen_width()));
   painter->drawRect(contentRect);
   painter->restore();
